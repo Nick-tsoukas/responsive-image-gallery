ad2dddad6684861115045f06bb67a64efa1fde7a
Nick-tsoukas <nick.tsoukas101@gmail.com>
2017-09-17 16:26:47 -0600

    added hide and show thumbnail functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 var DETAIL_IMAGE_SELECTOR =  '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]' ;
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
-
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var ESC_KEY = 27;
 function setDetails(imageUrl, titleText){
   'use strict';
   var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
@@ -26,11 +27,26 @@ function setDetailsFromThumb(thumbnail) {
   setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
 }
 
+
+//test function
+function showAlert(event) {
+  console.log('Yo! What Up');
+}
+
+function initEl() {
+  var el = document.getElementById("foo");
+  console.log(el);
+  el.onclick == showAlert;
+}
+
+
+
 function addThumbClickHandler(thumb) {
   'use strict';
   thumb.addEventListener('click' , function(event){
     event.preventDefault();
     setDetailsFromThumb(thumb);
+    showDetails();
   });
 }
 
@@ -41,10 +57,32 @@ function getThumbnailsArray() {
   return thumbnailArray;
 }
 
+function hideDetails() {
+  'use strict';
+  document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+
+function showDetails() {
+  'use strict';
+  document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+}
+
+function addKeyPressHandler() {
+  'use strict';
+  document.body.addEventListener('keyup', function(event) {
+    event.preventDefault();
+    console.log(event.keyCode);
+    if(event.keyCode === 27){
+      hideDetails();
+    }
+  });
+}
+
 function initializeEvents() {
   'use strict';
   var thumbnails = getThumbnailsArray();
   thumbnails.forEach(addThumbClickHandler);
+  addKeyPressHandler();
 }
 
 initializeEvents();
